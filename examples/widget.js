564876a4690a8c798c67c27e5d5e8f4ded9d8132
Subin Siby <subins2000@gmail.com>
2017-06-11 02:51:48 +0530

    Added theme option

--- examples/widget.js
+++ examples/widget.js
@@ -18,11 +18,14 @@
     var code = 'var cd = new CryptoDonate({\n';
     code += '  coin: "{coin}",\n';
     code += '  address: "{address}",\n';
+    code += '  buttonClass: "{theme}",\n';
+    code += '  dialogClass: "{theme}",\n';
     code += '});';
 
     function update() {
         var address = $('#address').val();
         var coin = $('#coin').val();
+        var theme = $('#theme').val();
 
         var cd = new CryptoDonate({
             coin: coin,
@@ -37,11 +40,12 @@
         var substitution = {
             coin: coin,
             address: address,
+            theme: theme
         };
         var keys = Object.keys(substitution);
 
         for (i = 0; i < keys.length; i++) {
-            widgetCode = widgetCode.replace('{' + keys[i] + '}', substitution[keys[i]]);
+            widgetCode = widgetCode.replace(new RegExp('{' + keys[i] + '}', 'g'), substitution[keys[i]]);
         }
 
         $('#code').html(widgetCode);
@@ -53,7 +57,7 @@
         $('#form select').material_select();
         $('#form select').on('change', update);
 
-        $('#code').click(function(){
+        $('#code').click(function() {
             SelectText(this);
         });
 
