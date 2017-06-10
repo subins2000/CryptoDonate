afeb3a0c5b189dd20b36be02954a2f14a6a717b5
Subin Siby <subins2000@gmail.com>
2017-06-10 20:21:34 +0530

    Changed file name of wallet icon

--- src/cryptodonate.js
+++ src/cryptodonate.js
@@ -79,7 +79,7 @@
 
                 dialog.innerHTML = '<div id="cryptodonate-action"></div>';
                 dialog.innerHTML += '<p id="cryptodonate-helper"></p>';
-                dialog.innerHTML += '<div id="cryptodonate-addressHolder"><img id="cryptodonate-coin" /><input type="text" id="cryptodonate-address" onclick="this.select();" /><a id="cryptodonate-wallet" target="_blank" href="" title="' + this.config.strings.openInWallet + '"><img src="' + this.config.baseURL + '/img/icon_wallet.png" /></a></div>';
+                dialog.innerHTML += '<div id="cryptodonate-addressHolder"><img id="cryptodonate-coin" /><input type="text" id="cryptodonate-address" onclick="this.select();" /><a id="cryptodonate-wallet" target="_blank" href="" title="' + this.config.strings.openInWallet + '"><img src="' + this.config.baseURL + '/img/wallet.png" /></a></div>';
                 dialog.innerHTML += '<div id="cryptodonate-qrHolder"><img id="cryptodonate-qr"></img></div>';
                 dialog.innerHTML += '<a id="cryptodonate-credit" href="https://subinsb.com/cryptodonate" target="_blank">CryptoDonate</a>';
                 dialog.innerHTML += '<a id="cryptodonate-close">x</a>'
