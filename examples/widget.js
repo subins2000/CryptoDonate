4249c613d8af8f695fa019a36ff768ecd0d82e96
Subin Siby <subins2000@gmail.com>
2017-06-11 13:43:53 +0530

    Show async JS code

--- examples/widget.js
+++ examples/widget.js
@@ -15,12 +15,16 @@
         }
     }
 
-    var code = 'var cd = new CryptoDonate({\n';
-    code += '  coin: "{coin}",\n';
-    code += '  address: "{address}",\n';
-    code += '  buttonClass: "{theme}",\n';
-    code += '  dialogClass: "{theme}",\n';
-    code += '});';
+    var code = '&lt;script&gt;\n';
+    code += '  !function(c){var t=document.createElement("script");t.type="text/javascript",t.async=!0,t.onload=c,t.src="//lab.subinsb.com/projects/Francium/cryptodonate/cryptodonate.min.js";var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}(function(){\n';
+    code += '    var cd = new CryptoDonate({\n';
+    code += '      coin: "{coin}",\n';
+    code += '      address: "{address}",\n';
+    code += '      buttonClass: "{theme}",\n';
+    code += '      dialogClass: "{theme}",\n';
+    code += '    });\n';
+    code += '  });\n';
+    code += '&lt;/script&gt;';
 
     function update() {
         var address = $('#address').val();
